7bbd29a3a0467ac87ab77881d689a5bf3c7044f1
Arudmin <rubicomspb@gmail.com>
2015-03-11 05:30:05 +0300

    fix UI

--- app/templates/init.js
+++ app/templates/init.js
@@ -3,5 +3,8 @@
 
     $('.button-collapse').sideNav();
 
+    // Correct switching hidden elements
+    // $('.overlay').toggleClass('hidden');
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
